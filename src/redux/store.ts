59ab5ba7e053804e0eb0992407d8612cc0e1de00
anurag <anurag.katiyar@flydocs.aero>
2025-03-07 01:57:03 +0530

    feature: store-grid-done

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import storesReducer from "./storesSlice";
-import skusReducer from "./skusSlice";
-import planningReducer from "./planningSlice";
 
 export const store = configureStore({
   reducer: {
     stores: storesReducer,
-    skus: skusReducer,
-    planning: planningReducer,
   },
 });
 
